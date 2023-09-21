d4d8141333c07df184b4c207312214695de20a57
sivakottakki <140244083+sivakottakki@users.noreply.github.com>
2023-09-21 10:34:41 +0530

    Add files via upload
    Do the curd operations

---
+++ SpringCurdApplications/src/main/java/com/ssd/curdoperations/SpringCurdApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.curdoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCurdApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCurdApplicationsApplication.class, args);
+	}
+
+}
---
+++ SpringCurdApplications/src/test/java/com/ssd/curdoperations/SpringCurdApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.curdoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurdApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
